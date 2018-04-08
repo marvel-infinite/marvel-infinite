15e9b3aa6cfb2305829cb94f44f70aa3edeb27f3
harsh <38183654+marvel-infinite@users.noreply.github.com>
2018-04-08 18:53:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+function main(){
+    let logo = document.getElementById('logo');
+    logo.classList.add('addClassForAnimation');
+    looper();
+}
+document.addEventListener('DOMContentLoaded',main,false);
+
+
+
+function FactVides(name,image){
+    this.name = name;
+    this.image  = image;
+    let FVcont = document.createElement('div');
+    let FVimg = document.createElement("img");
+    let FVtitle = document.createElement('h2');
+    let FVSection = document.getElementById('FactVideosList');
+    let br = document.createElement('br');
+    FVcont.classList.add('factvideo');
+    FVimg.src = this.image;
+    FVtitle.innerHTML = this.name;
+    FVcont.appendChild(FVimg);
+    FVcont.appendChild(FVtitle);
+    FVSection.appendChild(br);
+    FVSection.appendChild(FVcont);
+}
+
+function looper(){
+    for(let i = 0; i<FactsVideoRecources.length;i++){
+        FactVides(FactsVideoRecources[i].name,FactsVideoRecources[i].src);
+        console.log(FactsVideoRecources[i]);
+    }
+}
+
+const FactsVideoRecources = [{
+                'name':'Marvel Phase 4',
+                'src':'facts/Marvel-Phase-4.jpg'
+            },{
+                'name':'Thor gets his hammer back',
+                'src':'facts/Thor%20gets%20his%20hammer%20back.jpg'
+            },{
+                'name':'What is Ragnarok',
+                'src':'facts/what is ragnarok.jpg'
+            },{
+                'name':'Complete History Of infinity stones',
+                'src':'facts/infinity stones.jpg'
+}
+
+];
